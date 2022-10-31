a57b651596233b54e786fd992311021fc06d5e87
Rranathunga <115976009+Rranathunga@users.noreply.github.com>
2022-10-31 14:02:07 +0530

    Revert "Update App.js"

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ import Album from '../App';
 class App extends Component {
     render() {
         return (
+            // <Router>
+            //     <Scene key="root">
+            //         <Scene key="loading" component={LoadingScene} initial={true} hideNavBar={true} ></Scene>
+            //         <Scene key="auth" component={AuthScene} hideNavBar={true} direction='vertical'></Scene>
+            //         <Scene key="dashboard" component={Dashboard} hideNavBar={true}></Scene>
+            //         <Scene key="home" component={Home} hideNavBar={true}></Scene>
+            //     </Scene>
+            // </Router>
             <NativeRouter>
                 <Route exact path="/" component={AuthScene} />
                 <Route  path="/home" component={Home} />
